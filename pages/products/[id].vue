f7d9322ae3200d6f4037a90ba8877c4ba5ab8c33
Asadul Islam Hamzah <68386760+asad-cuet@users.noreply.github.com>
2023-03-03 20:29:01 +0600

    error page added

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -14,6 +14,10 @@ const {id}=useRoute().params
 const url='https://fakestoreapi.com/products/'+id
 
 const {data:product}=await useFetch(url,{key:id})   // key is tellig the fetch function that the id is different from last call
+if(!product.value)
+{
+    throw createError({ statusCode:404 ,statusMessage: 'Product not found', fatal:true})
+}
 </script>
 
 <style scoped>
